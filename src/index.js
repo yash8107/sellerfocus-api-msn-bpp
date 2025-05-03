e8e2924247f63c61de2feca707fd38f4017d292f
yash8107 <yashsinghal8107@gmail.com>
2025-05-04 01:19:11 +0530

    trying to make it working domain endpoint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.get('/', (req,res) => {
     res.send("hello world");
